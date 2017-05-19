7b1ec8e550d87681093249abf8a719843be3df08
lasking88 <lasking88@naver.com>
2017-05-19 13:05:13 +0200

    Text extraction from image (integration with ocr)

--- AccountBook/app/src/main/java/com/ohbrothers/www/accountbook/controller/ReceiptFragment.java
+++ AccountBook/app/src/main/java/com/ohbrothers/www/accountbook/controller/ReceiptFragment.java
@@ -1,10 +1,80 @@
 package com.ohbrothers.www.accountbook.controller;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.ohbrothers.www.accountbook.R;
+import com.ohbrothers.www.accountbook.ocr.OcrCaptureActivity;
 
 /**
  * Created by jk on 4/19/17.
  */
 
-public class ReceiptFragment extends Fragment {
+public class ReceiptFragment extends Fragment implements View.OnClickListener{
+
+    private CompoundButton autoFocus;
+    private CompoundButton useFlash;
+    private TextView statusMessage;
+    private TextView textValue;
+
+    private static final int RC_OCR_CAPTURE = 9003;
+    private static final String TAG = "ReceiptFragment";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_receipt, container, false);
+
+        statusMessage = (TextView)view.findViewById(R.id.status_message);
+        textValue = (TextView)view.findViewById(R.id.text_value);
+
+        autoFocus = (CompoundButton) view.findViewById(R.id.auto_focus);
+        useFlash = (CompoundButton) view.findViewById(R.id.use_flash);
+
+        view.findViewById(R.id.read_text).setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.read_text) {
+            // launch Ocr capture activity.
+            Intent intent = new Intent(getActivity(), OcrCaptureActivity.class);
+            intent.putExtra(OcrCaptureActivity.AutoFocus, autoFocus.isChecked());
+            intent.putExtra(OcrCaptureActivity.UseFlash, useFlash.isChecked());
+
+            startActivityForResult(intent, RC_OCR_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == RC_OCR_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    String text = data.getStringExtra(OcrCaptureActivity.TextBlockObject);
+                    statusMessage.setText(R.string.ocr_success);
+                    textValue.setText(text);
+                    Log.d(TAG, "Text read: " + text);
+                } else {
+                    statusMessage.setText(R.string.ocr_failure);
+                    Log.d(TAG, "No Text captured, intent data is null");
+                }
+            } else {
+                statusMessage.setText(String.format(getString(R.string.ocr_error),
+                        CommonStatusCodes.getStatusCodeString(resultCode)));
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
